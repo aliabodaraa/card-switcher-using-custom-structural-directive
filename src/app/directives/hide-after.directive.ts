496680296552a4e56d7cc8345b9cc975b3168e35
Ali Abodaraa <abodaraaali50@gmail.com>
2024-03-14 17:39:34 +0300

    make usage od 'as' keyword

--- src/app/directives/hide-after.directive.ts
+++ src/app/directives/hide-after.directive.ts
@@ -6,22 +6,32 @@ import {
   ViewContainerRef,
 } from '@angular/core';
 
+class HideAfterContext {
+  public hideAfter = 0; //for using "as" keyword should match the directive input "hideAfter"
+}
+
 @Directive({
   selector: '[hideAfter]',
 })
 export class HideAfterDirective implements OnInit {
-  @Input('hideAfter') delay = 0;
+  private _delay = 0;
+  @Input('hideAfter') set delay(value: number | null) {
+    this._delay = value ?? 0;
+    this.context.hideAfter = this._delay / 1000;
+  }
   @Input('hideAfterThen') placeholder: TemplateRef<any> | null = null;
+  private context = new HideAfterContext();
+
   constructor(
     private view_container: ViewContainerRef,
     private template: TemplateRef<any>
   ) {}
   ngOnInit(): void {
-    this.view_container.createEmbeddedView(this.template);
+    this.view_container.createEmbeddedView(this.template, this.context);
     setTimeout(() => {
       this.view_container.clear();
       if (this.placeholder)
         this.view_container.createEmbeddedView(this.placeholder);
-    }, this.delay);
+    }, this._delay);
   }
 }

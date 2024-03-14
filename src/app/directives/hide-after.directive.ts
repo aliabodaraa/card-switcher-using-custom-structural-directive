42058e111999b7b5f6cc2d002fde69a3ea1eba7a
Ali Abodaraa <abodaraaali50@gmail.com>
2024-03-14 17:54:24 +0300

    use context with strict typing

--- src/app/directives/hide-after.directive.ts
+++ src/app/directives/hide-after.directive.ts
@@ -7,7 +7,11 @@ import {
 } from '@angular/core';
 
 class HideAfterContext {
+  public get $implicit() {
+    return this.hideAfter;
+  }
   public hideAfter = 0; //for using "as" keyword should match the directive input "hideAfter"
+  public counter = 0;
 }
 
 @Directive({
@@ -17,21 +21,26 @@ export class HideAfterDirective implements OnInit {
   private _delay = 0;
   @Input('hideAfter') set delay(value: number | null) {
     this._delay = value ?? 0;
-    this.context.hideAfter = this._delay / 1000;
+    this.context.hideAfter = this.context.counter = this._delay / 1000;
   }
-  @Input('hideAfterThen') placeholder: TemplateRef<any> | null = null;
+  @Input('hideAfterThen') placeholder: TemplateRef<HideAfterContext> | null =
+    null;
   private context = new HideAfterContext();
 
   constructor(
     private view_container: ViewContainerRef,
-    private template: TemplateRef<any>
+    private template: TemplateRef<HideAfterContext>
   ) {}
   ngOnInit(): void {
     this.view_container.createEmbeddedView(this.template, this.context);
+    const counter_interval_id = setInterval(() => {
+      this.context.counter--;
+    }, 1000);
     setTimeout(() => {
       this.view_container.clear();
       if (this.placeholder)
-        this.view_container.createEmbeddedView(this.placeholder);
+        this.view_container.createEmbeddedView(this.placeholder, this.context);
+      clearInterval(counter_interval_id);
     }, this._delay);
   }
 }

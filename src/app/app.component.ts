33445e9933fa604ddb2a4bdb3588c57ac1b1c575
Ali Abodaraa <abodaraaali50@gmail.com>
2024-03-15 12:08:15 +0300

    apply if-loaded custom structural direcive with type narrowing for state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { LoadingState } from './directives/if-loaded.directive';
+export type Person = { name: string };
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <section *ifLoaded="state; else alt_tmp">
+      Data was Loaded . User name:{{ state.data.name }}
+    </section>
+    <ng-template #alt_tmp>alternative_tmp {{ state.type }}</ng-template>
+  `,
 })
 export class AppComponent {
-  title = 'customFeatures';
+  state: LoadingState<Person> = {
+    type: 'loading',
+  };
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.state = {
+        type: 'loaded',
+        data: { name: 'Ali Abodaraa' },
+      };
+    }, 2000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HideAfterDirective } from './directives/hide-after.directive';
+import { IfLoadedDirective } from './directives/if-loaded.directive';
 
 @NgModule({
-  declarations: [AppComponent, HideAfterDirective],
+  declarations: [AppComponent, IfLoadedDirective],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],

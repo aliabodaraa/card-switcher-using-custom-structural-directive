adca1ddb0f86cdd08b35e7c1caa5ceec76cb3e59
aliabodaraa <abodaraaali50@gmail.com>
2024-07-24 10:38:16 +0300

    multicasting forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { interval, from, of, Observable, fromEvent, merge, EMPTY } from 'rxjs';
+import {
+  interval,
+  from,
+  of,
+  Observable,
+  fromEvent,
+  merge,
+  EMPTY,
+  Subject,
+  ConnectableObservable,
+} from 'rxjs';
 import {
   reduce,
   scan,
@@ -11,6 +21,9 @@ import {
   switchMapTo,
   mapTo,
   startWith,
+  share,
+  multicast,
+  refCount,
 } from 'rxjs/operators';
 const COUNTER_FROM = 10;
 @Component({
@@ -26,32 +39,54 @@ export class AppComponent implements AfterViewInit {
   counter$ = interval(1000);
 
   ngAfterViewInit() {
-    merge(
-      fromEvent<MouseEvent>(this.startBtn.nativeElement, 'click').pipe(
-        mapTo(true)
-      ),
-      fromEvent<MouseEvent>(this.pauseBtn.nativeElement, 'click').pipe(
-        mapTo(false)
-      )
-    )
-      .pipe(
-        switchMap((shouldStart) => (shouldStart ? this.counter$ : EMPTY)),
-        mapTo(-1),
-        scan((accumulator, current) => {
-          return accumulator + current;
-        }, COUNTER_FROM),
-        takeWhile((value) => value >= 0),
-        startWith(COUNTER_FROM),
-        tap(console.log)
-      )
-      .subscribe({
-        next: (value: number) => {
-          this.h1.nativeElement.innerText = value;
-          if (!value) {
-            this.h1.nativeElement.innerText = 'Lift Off';
-          }
-        },
-        complete: () => console.log('!COMPLETE'),
-      });
+    //old approach
+    let obs$ = interval(1000).pipe(
+      tap((x) => console.log(x, '   some side effect old approach')),
+      multicast(() => new Subject())
+    );
+    let connectedSub = (obs$ as ConnectableObservable<number>).connect(); //start subscribing to connectableObservable
+    let sub1 = obs$.subscribe((x) => console.log('SUBSCRIBER 1 old approach'));
+    let sub2 = obs$.subscribe((x) => console.log('SUBSCRIBER 2 old approach'));
+
+    setTimeout(() => {
+      connectedSub.unsubscribe(); //we need to unsubscribe from connectedObservable because it will still emitting values although unsubscribing from sub1 and sub2
+      sub1.unsubscribe();
+      sub2.unsubscribe();
+    }, 2000);
+
+    //new approach
+    let obs_new$ = interval(1000).pipe(
+      tap((x) => console.log(x, '   some side effect new approach')),
+      multicast(() => new Subject()),
+      refCount() //manage the connect and unsubscribe proccesses from connectableObservable internally for us
+    );
+    let sub1_new = obs_new$.subscribe((x) =>
+      console.log('SUBSCRIBER 1 new approach')
+    );
+    let sub2_new = obs_new$.subscribe((x) =>
+      console.log('SUBSCRIBER 2 new approach')
+    );
+
+    setTimeout(() => {
+      sub1_new.unsubscribe();
+      sub2_new.unsubscribe();
+    }, 2000);
+
+    //recent approach
+    let obs_recent$ = interval(1000).pipe(
+      tap((x) => console.log(x, '   some side effect recent approach')),
+      share() //do multicast and refCount behind the scene
+    );
+    let sub1_recent = obs_recent$.subscribe((x) =>
+      console.log('SUBSCRIBER 1 recent approach')
+    );
+    let sub2_recent = obs_recent$.subscribe((x) =>
+      console.log('SUBSCRIBER 2 recent approach')
+    );
+
+    setTimeout(() => {
+      sub1_recent.unsubscribe();
+      sub2_recent.unsubscribe();
+    }, 2000);
   }
 }
